f2f21264c8fe52a7d844cb70fbcbd383718ac1a0
sebastien reynaud <sebcbien85540@gmail.com>
2024-11-13 11:43:23 +0100

    add contact page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import {HomePageComponent} from "./components/home-page/home-page.component";
 import {LoginComponent} from "./components/login/login.component";
 import {RegisterComponent} from "./components/register/register.component";
 import {ProductDetailComponent} from "./components/product-detail/product-detail.component";
+import {ContactComponent} from "./components/contact/contact.component";
 
 export const routes: Routes = [
   {path: '', component: HomePageComponent},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'product/:id', component: ProductDetailComponent},
+  {path:'contact', component: ContactComponent},
 ];
